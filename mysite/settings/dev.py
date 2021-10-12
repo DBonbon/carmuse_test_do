269685dd14ac68984579134434cc0e2a6b46bcae
Yariv Friedman <yrfriedman@gmail.com>
2021-10-12 10:45:58 +0200

    initial commit

---
+++ mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-ds2)dtoc%uz=_=aap*y-@&i*7%z=h-(&mjy-$-e2dre0aesmzz'
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['*'] 
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
